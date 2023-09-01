8b3f9a3e723d86320c9ce7e8911f69c7c0482740
devendra-alt <devendramulewa171@gmail.com>
2023-09-01 22:43:46 +0530

    Added userInput handler module

---
+++ src/modules/userInput.js
@@ -0,0 +1,27 @@
+import { addQuestionElement } from './conversation.js';
+
+const userInputField = document.querySelector('#generative-ai-query-input');
+const generativeAiQueryInfo = document.querySelector(
+  '.generative-ai-query-info'
+);
+
+const userInput = (event, generativeAi) => {
+  //* * default operations */
+  event.preventDefault();
+  generativeAiQueryInfo.ariaHidden = true;
+  generativeAiQueryInfo.ariaInvalid = false;
+
+  const userInputValue = userInputField.value.trim().toLowerCase();
+  userInputField.value = '';
+
+  if (!userInputValue) {
+    generativeAiQueryInfo.textContent = 'No Message found';
+    generativeAiQueryInfo.ariaHidden = false;
+    generativeAiQueryInfo.ariaInvalid = true;
+    return;
+  }
+  addQuestionElement(userInputValue);
+  generativeAi.getResponse(userInputValue);
+};
+
+export default userInput;
